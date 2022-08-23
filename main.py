38547c7a2cec7b75ff0176c967a5389b03ce4927
WinSion <50723356+HelloWinSion@users.noreply.github.com>
2022-08-24 00:55:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 # 获取随机颜色
-  get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+ def get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
   color_list = get_colors(100)
   return random.choice(color_list)
 
