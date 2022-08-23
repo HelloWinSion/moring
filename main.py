5960712864a5882a98531e252064a0b6b4551ff4
WinSion <50723356+HelloWinSion@users.noreply.github.com>
2022-08-24 00:58:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,10 +43,11 @@ def get_words():
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
-# 获取随机颜色
- def get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-  color_list = get_colors(100)
-  return random.choice(color_list)
+def get_color():
+    # 获取随机颜色
+    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
+    color_list = get_colors(100)
+    return random.choice(color_list)
 
 
 client = WeChatClient(app_id, app_secret)
